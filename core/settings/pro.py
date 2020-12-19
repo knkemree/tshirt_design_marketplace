d099b116666269d66c248d4e15463adb8a3ef7ec
knkemree <konakziyaemre@gmail.com>
2020-12-19 13:22:04 -0600

    aws

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -31,7 +31,7 @@ DATABASES = {
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'contextcustom'
-AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
+AWS_S3_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
