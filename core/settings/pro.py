cad1dd10f9ee213a941373e279ea0c291031e075
knkemree <konakziyaemre@gmail.com>
2020-12-19 16:46:42 -0600

    aws

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -47,7 +47,7 @@ AWS_DEFAULT_ACL = 'public-read'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = 'static/'
+#STATIC_ROOT = 'static_cdn/'
 
 CELERY_BROKER_URL='amqp://localhost'
 CELERY_RESULT_BACKEND='amqp://localhost'
