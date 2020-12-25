c389546de255bbd5061260267526baf946c6ed55
knkemree <konakziyaemre@gmail.com>
2020-12-25 13:32:19 -0600

    aws object storage

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -36,7 +36,7 @@ DATABASES = {
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-AWS_STORAGE_BUCKET_NAME = 'contextcustom'
+AWS_STORAGE_BUCKET_NAME = 'contextcustom-space'
 AWS_S3_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
