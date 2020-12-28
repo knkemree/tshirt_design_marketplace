1c3e8e1367eb0bd4999525a4f784dd38521a3e00
knkemree <konakziyaemre@gmail.com>
2020-12-28 10:08:09 -0600

    size

--- essentials/views.py
+++ essentials/views.py
@@ -56,7 +56,7 @@ def product_detail(request, id, slug):
         try:
             sizes = Variant.objects.raw('SELECT * FROM  essentials_variant  WHERE product_id=%s GROUP BY size_id, id',[id])
         except:
-            sizes = Variant.objects.distinct('size')
+            sizes = Variant.objects.distinct('size_id')
         query += variant.product.title+' Size:' +str(variant.size) +' Color:' +str(variant.color)
         print(query, 'queryde ne var')
     else:
