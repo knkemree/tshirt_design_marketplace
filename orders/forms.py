0d1b7aa77fb11f11a61a8382c3f410773c9b638d
knkemree <konakziyaemre@gmail.com>
2021-01-07 13:24:42 -0600

    change

--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Order
 class OrderCreateForm(forms.ModelForm):
+    recipient = forms.CharField(required=False)
+
     class Meta:
         model = Order
         fields = ['recipient','shipping_label']
