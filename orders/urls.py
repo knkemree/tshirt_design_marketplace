12252e0c77d5d073d4758a9a26528b0989450863
knkemree <konakziyaemre@gmail.com>
2021-01-18 14:00:07 -0600

    create invoice

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     #path('order/<int:order_id>/', views.order_detail, name='order_detail'),
     path('create/', views.order_create, name='order_create'),
     path('admin/order/<int:order_id>/', views.admin_order_detail, name='admin_order_detail'),
-    #path('admin/order/<int:order_id>/pdf/',views.admin_order_pdf,name='admin_order_pdf'),
+    path('admin/order/<int:order_id>/pdf/',views.admin_order_pdf,name='admin_order_pdf'),
 ]
