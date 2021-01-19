12e08b0ac62f4cc27bbf8568cb59351172d660d5
knkemree <konakziyaemre@gmail.com>
2021-01-19 13:15:40 -0600

    size issue

--- essentials/views.py
+++ essentials/views.py
@@ -70,10 +70,9 @@ def product_detail(request, id, slug):
 
 def ajaxcolor(request):
     data = {}
-    if request.is_ajax:
-        size_id = request.GET.get('size_id', None)
-        productid = request.GET.get('product_id', None)
-        print(size_id, productid)
+    if request.POST.get('action') == 'post':
+        size_id = request.POST.get('size')
+        productid = request.POST.get('productid')
         colors = Variant.objects.filter(product_id=productid, size_id=size_id)
         ajax_variant = colors[0]
         context = {
