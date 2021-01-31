8360ecdca090f3dd797f17f241ca10d5d5d2494c
knkemree <konakziyaemre@gmail.com>
2021-01-31 02:20:01 -0600

    added recommended products to design page

--- essentials/views.py
+++ essentials/views.py
@@ -68,7 +68,7 @@ def product_detail(request, id, slug):
         qty = 7
     else:
         qty = len(others)
-    others = random.sample(others, 7)
+    others = random.sample(others, qty)
 
     #color = colors[0].color.name
     
