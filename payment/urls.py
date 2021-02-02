c842813e5237c3d1f19fd75de9c072e694359376
knkemree <konakziyaemre@gmail.com>
2021-02-02 08:27:58 -0600

    added pay now for not paid orders in order history

--- payment/urls.py
+++ payment/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'payment'
 urlpatterns = [
     path('process/', views.payment_process, name='process'),
+    path('pay_order/<int:id>', views.pay_order, name='pay_order'),
     path('done/', views.payment_done, name='done'),
     path('canceled/', views.payment_canceled, name='canceled'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -64,3 +64,43 @@ def payment_done(request):
     return render(request, 'done.html')
 def payment_canceled(request):
     return render(request, 'canceled.html')
+
+
+def pay_order(request, id):
+    order = get_object_or_404(Order, id=id)
+    total_cost = order.get_total_cost()
+    if request.method == 'POST':
+        # retrieve token
+        token = request.POST.get('stripeToken')
+        # create and submit transaction
+        result = stripe.Charge.create(
+            #amount=100,
+            amount=int(total_cost*100),
+            currency="usd",
+            #customer = order.ordered_by.seller.seller.stripe_id,
+            source=token,
+            description="Payment for Context Custom by {}".format(order.ordered_by),
+        )
+        print("result burda")
+        print(result)
+        if result.status == "succeeded":
+            # mark the order as paid
+            order.paid = True
+            # store the unique transaction id
+            order.stripe_id = result.id
+            order.save()
+            subject = "New Order"
+            message = "Order"
+            #mail_admins(subject, message, html_message="{} has just placed an order. <a href='https://contextcustom.com/admin/orders/order/{}/change/'>Check Now!</a>".format(order.customer_name(), order.id))
+            # cart.clear()
+            # launch asynchronous task
+            # payment_completed.delay(order.id)
+            return redirect('payment:done')
+        else:
+            return redirect('payment:canceled')
+    else:
+        # generate token
+        return render(request,
+                      'process.html',
+                      {'order': order,
+                       'publish_key':STRIPE_PUBLIC_KEY})
