96654a973854547e5766875b98a725fc2ab1753c
knkemree <konakziyaemre@gmail.com>
2021-02-03 17:05:04 -0600

    product admin list diaplay

--- essentials/admin.py
+++ essentials/admin.py
@@ -85,7 +85,7 @@ class PlacementInline(admin.TabularInline):
 
 @admin_thumbnails.thumbnail('image')
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','active','image_tag']
+    list_display = ['id','image_tag','title','brand','variant_type','active','created_at','updated_at']
     list_filter = ['category']
     #readonly_fields = ('image_tag',)
     inlines = [VariantInline, 
