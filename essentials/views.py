1ad18fd6f62d28b54d4234099eb7af6bc77b6c30
knkemree <konakziyaemre@gmail.com>
2021-02-03 17:28:57 -0600

    order_by remove

--- essentials/views.py
+++ essentials/views.py
@@ -42,7 +42,7 @@ def product_list(request, category_slug=None):
     categories = Category.objects.filter(active=True)
     parent_categories = Category.objects.filter(active=True, parent=None) 
 
-    products = Product.objects.filter(active=True).order_by('category')
+    products = Product.objects.filter(active=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
