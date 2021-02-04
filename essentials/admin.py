e8e59faf2035b26621f54e186690328ee78c154b
knkemree <konakziyaemre@gmail.com>
2021-02-04 09:51:31 -0600

    some changes on brnds

--- essentials/admin.py
+++ essentials/admin.py
@@ -85,9 +85,13 @@ class PlacementInline(admin.TabularInline):
 
 @admin_thumbnails.thumbnail('image')
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id','image_tag','title','brand','variant_type','active','created_at','updated_at']
+    list_display = ['id','image_tag','title',
+    'brand',
+    'variant_type','active','created_at','updated_at']
     list_filter = ['category']
-    list_editable = ['brand','active']
+    list_editable = [
+        'brand',
+        'active']
     #readonly_fields = ('image_tag',)
     inlines = [VariantInline, 
     MethodInline, 
