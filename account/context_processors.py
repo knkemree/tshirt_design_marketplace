4c239965beab82a1a10f04b1d84dce59dafd951b
knkemree <konakziyaemre@gmail.com>
2021-02-05 07:06:33 -0600

    change on something

--- account/context_processors.py
+++ account/context_processors.py
@@ -2,11 +2,8 @@ from django.shortcuts import get_object_or_404
 from account.models import Customer, Seller
 
 def credit_amount(request):
-    try:
-        seller = Seller.objects.get(seller=request.user)
-        credit_amount = seller.credit
-        context = {'credit_amount':credit_amount}
-        print(credit_amount, 'ne kadar kredisi var')
-        return context
-    except:
-        return {'credit_amount':0}
+    seller = Seller.objects.get(seller=request.user)
+    credit_amount = seller.credit
+    context = {'credit_amount':credit_amount}
+    print(credit_amount, 'ne kadar kredisi var')
+    return context
