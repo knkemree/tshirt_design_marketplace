17a27a13ebd3b64b76d9dc2857b57af9559ac5fc
knkemre <konakziyaemre@gmail.com>
2021-03-11 17:56:14 -0600

    show payment errors on dif page, prevent duplicate

--- payment/urls.py
+++ payment/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('pay_for_credit/', views.pay_for_credit, name='pay_for_credit'),
     path('pay_order/<int:id>', views.pay_order, name='pay_order'),
     path('done/', views.payment_done, name='done'),
+    path('pay_order_done/', views.pay_order_done, name='pay_order_done'),
     path('canceled/', views.payment_canceled, name='canceled'),
+    path('credit_payment_failed/', views.credit_payment_failed, name='credit_payment_failed'),   
 ]
--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,7 @@ import stripe
 from decimal import Decimal
 from decouple import config
 from django.urls import reverse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.debug import sensitive_variables
 from django.shortcuts import render, redirect, get_object_or_404
@@ -21,9 +22,14 @@ STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
 @sensitive_variables('token')
 def payment_process(request):
     cart = Cart(request)
-    
-    order_id = request.session.get('order_id')
-    order = get_object_or_404(Order, id=order_id)
+
+    #eger order'i bulamazsa order list'e geri don. order'i bulamamasinin sebebi session'dan order_id'nin silinmesi olabilir
+    try:
+        order_id = request.session.get('order_id')
+        order = Order.objects.get(id=order_id)
+    except:
+        return redirect("orders:order_list")
+
     total_cost = order.get_total_cost()
     credit = order.ordered_by.credit
         
@@ -34,19 +40,15 @@ def payment_process(request):
         # retrieve token
         token = request.POST.get('stripeToken')
         # create and submit transaction
-        result = stripe.Charge.create(
-            #amount=100,
-            amount=int(after_credit*100),
-            currency="usd",
-            #customer = order.ordered_by.seller.stripe_id,
-            source=token,
-            description="Customer:{}, Order #{}, Total:${}, Used credit:${}, Paid Amount: ${}".format(order.ordered_by, order.id, total_cost, credit, after_credit),
-        )
-        print("result burda")
-        print(result)
-        del request.session['order_id']
-        request.session.modified = True
-        if result.status == "succeeded":
+        try:
+            result = stripe.Charge.create(
+                #amount=100,
+                amount=int(after_credit*100),
+                currency="usd",
+                #customer = order.ordered_by.seller.stripe_id,
+                source=token,
+                description="Customer:{}, Order #{}, Total:${}, Used credit:${}, Paid Amount: ${}".format(order.ordered_by, order.id, total_cost, credit, after_credit),
+            )
             # mark the order as paid
             order.paid = True
             # store the unique transaction id
@@ -54,22 +56,55 @@ def payment_process(request):
             subject = "New Order"
             message = "Order"
             mail_admins(subject, message, html_message="{} has just placed an order. <a href='https://contextcustom.com/admin/orders/order/{}/change/'>Check Now!</a>".format(order.customer_name(), order.id))
-
-
             order.ordered_by.credit = 0 
-            #credit_record = Credit.objects.create(user=order.ordered_by, order_id=order.id, amount=credit*-1)
             order.save()
             order.ordered_by.save()
-            #credit_record.save()
-            print(after_credit, 'ne kadar charge edildi')
             # cart.clear()
             # launch asynchronous task
             # payment_completed.delay(order.id)
-            return redirect('payment:done')
-        else:
             del request.session['order_id']
             request.session.modified = True
+            return redirect('payment:done')
+
+        except stripe.error.CardError as e:
+            body = e.json_body
+            err = body.get('error', {})
+            messages.error(request, f"{err.get('message')}")
             return redirect('payment:canceled')
+
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            messages.error(request, "To many request error")
+            return redirect('payment:canceled')
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            messages.error(request, "Invalid Parameter")
+            return redirect('payment:canceled')
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            messages.error(request, "Authentication with stripe failed")
+            return redirect('payment:canceled')
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            messages.error(request, "Network Error")
+            return redirect('payment:canceled')
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            messages.error(request, "Something went wrong")
+            return redirect('payment:canceled')
+        
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            messages.error(request, "Not identified error")
+
+            return redirect('payment:canceled')
+
     else:
 
         if request.method == 'POST' and total_cost <= credit:
@@ -100,12 +135,29 @@ def payment_process(request):
 
 
 @login_required(login_url='/signup/')
+def pay_order_done(request):
+    return render(request, 'pay_order_done.html')
+
+@login_required(login_url='/signup/')
 def payment_done(request):
     return render(request, 'done.html')
 
 @login_required(login_url='/signup/')
 def payment_canceled(request):
-    return render(request, 'canceled.html')
+    #eger order'i bulamazsa order list'e geri don. order'i bulamamasinin sebebi session'dan order_id'nin silinmesi olabilir
+    try:
+        order_id = request.session.get('order_id')
+        order = Order.objects.get(id=order_id) 
+        context = {'order_id':order.id}
+        return render(request, 'canceled.html', context)
+    except:
+        return redirect("orders:order_list")
+
+@login_required(login_url='/signup/')
+def credit_payment_failed(request):
+    return render(request, 'credit_payment_failed.html',)
+    
+    
 
 
 @login_required(login_url='/signup/')
@@ -118,38 +170,69 @@ def pay_order(request, id):
         
         # retrieve token
         token = request.POST.get('stripeToken')
-        # create and submit transaction
-        result = stripe.Charge.create(
-            #amount=100,
-            amount=int(after_credit*100),
-            currency="usd",
-            #customer = order.ordered_by.seller.stripe_id,
-            source=token,
-            description="Customer:{}, Order #{}, Total:${}, Used credit:${}, Paid Amount: ${}".format(order.ordered_by, order.id, total_cost, credit, after_credit),
-        )
-        print("result burda")
-        print(result)
-        if result.status == "succeeded":
+        try:
+            result = stripe.Charge.create(
+                #amount=100,
+                amount=int(after_credit*100),
+                currency="usd",
+                #customer = order.ordered_by.seller.stripe_id,
+                source=token,
+                description="Customer:{}, Order #{}, Total:${}, Used credit:${}, Paid Amount: ${}".format(order.ordered_by, order.id, total_cost, credit, after_credit),
+            )
             # mark the order as paid
             order.paid = True
             # store the unique transaction id
             order.stripe_id = result.id
-            subject = "Order Paid"
+            subject = "New Order"
             message = "Order"
-            mail_admins(subject, message, html_message="{} has just made a payment for order #{}. <a href='https://contextcustom.com/admin/orders/order/{}/change/'>Check Now!</a>".format(order.customer_name(), order.id, order.id))
-
-
+            mail_admins(subject, message, html_message="{} has just placed an order. <a href='https://contextcustom.com/admin/orders/order/{}/change/'>Check Now!</a>".format(order.customer_name(), order.id))
             order.ordered_by.credit = 0 
-            #credit_record = Credit.objects.create(user=order.ordered_by, order_id=order.id, amount=credit*-1)
             order.save()
             order.ordered_by.save()
-            #credit_record.save()
-            print(after_credit, 'ne kadar charge edildi')
             # cart.clear()
             # launch asynchronous task
             # payment_completed.delay(order.id)
+            del request.session['order_id']
+            request.session.modified = True
             return redirect('payment:done')
-        else:
+
+        except stripe.error.CardError as e:
+            body = e.json_body
+            err = body.get('error', {})
+            messages.error(request, f"{err.get('message')}")
+            return redirect('payment:canceled')
+
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            messages.error(request, "To many request error")
+            return redirect('payment:canceled')
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            messages.error(request, "Invalid Parameter")
+            return redirect('payment:canceled')
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            messages.error(request, "Authentication with stripe failed")
+            return redirect('payment:canceled')
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            messages.error(request, "Network Error")
+            return redirect('payment:canceled')
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            messages.error(request, "Something went wrong")
+            return redirect('payment:canceled')
+        
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            messages.error(request, "Not identified error")
+
             return redirect('payment:canceled')
     else:
 
@@ -191,20 +274,18 @@ def pay_for_credit(request):
 
         # retrieve token
         token = request.POST.get('stripeToken')
-        # create and submit transaction
-        result = stripe.Charge.create(
-            #amount=100,
-            amount=int(int(amount)*100),
-            currency="usd",
-            #customer = order.ordered_by.seller.stripe_id,
-            source=token,
-            description="Customer uploaded money",
-        )
-        del request.session['amount']
-        request.session.modified = True
-        
-        
-        if result.status == "succeeded":
+        try:
+            # create and submit transaction
+            result = stripe.Charge.create(
+                #amount=100,
+                amount=int(int(amount)*100),
+                currency="usd",
+                #customer = order.ordered_by.seller.stripe_id,
+                source=token,
+                description="Customer uploaded money",
+            )
+
+
 
             credit = Credit.objects.create(email=seller, added_amount=amount, created_by = 'seller')
             seller.credit = seller.credit + Decimal(credit.added_amount)
@@ -212,16 +293,52 @@ def pay_for_credit(request):
             # store the unique transaction id
             credit.stripe_id = result.id
             credit.save()
-            #subject = "Order Paid"
-            #message = "Order"
-            #mail_admins(subject, message, html_message="{} has just made a payment for order #{}. <a href='https://contextcustom.com/admin/orders/order/{}/change/'>Check Now!</a>".format(order.customer_name(), order.id, order.id))
-            
+            subject = "Credit Given"
+            message = "Credit"
+            mail_admins(subject, message, html_message="{} has just uploaded credits. <a href='https://contextcustom.com/admin/account/credit/{}/change/'>Check Now!</a>".format(credit.email, credit.id))
+            del request.session['amount']
+            request.session.modified = True
             return redirect('payment:done')
 
-        
+        except stripe.error.CardError as e:
+            body = e.json_body
+            err = body.get('error', {})
+            messages.error(request, f"{err.get('message')}")
+            return redirect('payment:credit_payment_failed')
 
-        else:
-            return redirect('payment:canceled')
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            messages.error(request, "To many request error")
+            return redirect('payment:credit_payment_failed')
+
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            messages.error(request, "Invalid Parameter")
+            return redirect('payment:credit_payment_failed')
+
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            messages.error(request, "Authentication with stripe failed")
+            return redirect('payment:credit_payment_failed')
+
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            messages.error(request, "Network Error")
+            return redirect('payment:credit_payment_failed')
+
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            messages.error(request, "Something went wrong")
+            return redirect('payment:credit_payment_failed')
+        
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            messages.error(request, "Not identified error")
+
+            return redirect('payment:credit_payment_failed')
+        
     elif amount == None:
             return redirect(reverse('orders:order_list'))
     else:
